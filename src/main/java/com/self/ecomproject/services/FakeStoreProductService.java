67efcf0d8579902286150d8a6a2a5128e5685e69
Pranmay <pranmayj09@gmail.com>
2024-04-06 21:18:10 +0530

    Implemented 3rd party API to get product by id

--- src/main/java/com/self/ecomproject/services/FakeStoreProductService.java
+++ src/main/java/com/self/ecomproject/services/FakeStoreProductService.java
@@ -1,5 +1,7 @@
 package com.self.ecomproject.services;
 
+import com.self.ecomproject.dtos.FakeStoreProductDTO;
+import com.self.ecomproject.models.Category;
 import com.self.ecomproject.models.Product;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -7,11 +9,39 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 @Service
 public class FakeStoreProductService implements ProductService{
+    private RestTemplate restTemplate;
+    public FakeStoreProductService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    private Product covertFakeStoreProductDtoToProduct(FakeStoreProductDTO dto) {
+        Product product = new Product();
+        product.setId(dto.getId());
+        product.setTitle(dto.getTitle());
+        product.setPrice(dto.getPrice());
+        product.setDescription(dto.getDesc());
+        product.setImage(dto.getImage());
+
+        Category category = new Category();
+        category.setDescription(dto.getDesc());
+
+        product.setCategory(category);
+
+        return product;
+    }
     @Override
     public Product getProductById(Long id) {
         //Call FakeStore API here to get product with given ID
-        RestTemplate restTemplate = new RestTemplate();
-        return new Product();
+        FakeStoreProductDTO fakeStoreProductDTO = restTemplate.getForObject("https://fakestoreapi.com/products/" + id,
+                FakeStoreProductDTO.class);
+        //first param -> URL
+        //second param -> Response
+
+        //Convert FakeStoreProductDTO into Product object.
+        if (fakeStoreProductDTO == null) {
+            return null;
+        }
+        return covertFakeStoreProductDtoToProduct(fakeStoreProductDTO);
     }
 
     @Override
